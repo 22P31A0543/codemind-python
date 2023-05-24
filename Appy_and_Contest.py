cb1f6e8b991ab3cc04bb068734d1b870bde9cd85
Yasaswini Vulisetti <119730496+22P31A0543@users.noreply.github.com>
2023-05-24 09:34:39 +0530

    import math t=int(input()) for k in range(t):     n,a,b,k=map(int,input().split())     p=n//a     q=n//b     c=math.gcd(a,b)     d=(a*b)//c     s=n//d     if((p+q-2*s)>=k):         print("Win")     else:         print("Lose")

--- Appy_and_Contest.py
+++ Appy_and_Contest.py
@@ -1,10 +1,13 @@
 import math
-for _ in range(int(input())):
-    n,x,y,k=[int(x) for x in input().split()]
-    a=int(n/x)
-    b=int(n/y)
-    c=int(n/((x*y)//math.gcd(x,y)))
-    if a+b-(2*c)>=k:
+t=int(input())
+for k in range(t):
+    n,a,b,k=map(int,input().split())
+    p=n//a
+    q=n//b
+    c=math.gcd(a,b)
+    d=(a*b)//c
+    s=n//d
+    if((p+q-2*s)>=k):
         print("Win")
     else:
         print("Lose")
